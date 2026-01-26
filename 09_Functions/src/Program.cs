c4c1865124eed96ef53ec1a3f89c92635609ee47
Tobias <Tobias.Boyke@proton.me>
2026-01-26 11:04:50 +0100

    feat: Implement console UI and program entry point to demonstrate various functions.

---
+++ 09_Functions/src/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace _09_Functions.src
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new ConsoleUI().Run();
+        }
+    }
+}
